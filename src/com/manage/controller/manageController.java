552e16040091fa4e1102760ca1dd0de26b829fde
ctyso <1204830623@qq.com>
2018-01-16 10:34:18 +0800

    控制器注释

--- src/com/manage/controller/manageController.java
+++ src/com/manage/controller/manageController.java
@@ -1,5 +1,16 @@
 package com.manage.controller;
    
+/**
+ * 控制器
+ * @Function TODO
+ * @Description       		
+ * @Copyright © 2018, www.ssit-xm.com.cn, All Rights Reserved
+ * @since 1.0   
+ * @version 1.0  
+ * @author luchunwen@ssit-xm.com.cn 卢春文 2018年1月16日 
+ * @see #log
+ */
 public class manageController {
+	
 
 }
