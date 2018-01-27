fb36eb4f056accb9f3d3cf4bd0ef45596ed62107
ctyso <1204830623@qq.com>
2018-01-27 17:39:34 +0800

    数据库表设计

--- src/com/manage/user/service/impl/UserService.java
+++ src/com/manage/user/service/impl/UserService.java
@@ -1,18 +1,23 @@
 package com.manage.user.service.impl;
 
+import com.manage.base.MyBatisDao;
+import com.manage.user.domain.User;
+import com.manage.user.service.IUserService;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
-import com.manage.user.domain.User;
-import com.manage.user.service.IUserService;
-
 @Service
 @Transactional
-public class UserService implements IUserService  {
+public class UserService extends MyBatisDao implements IUserService  {
+	
 
+	
 	@Override
 	public User loginUser() {
-		System.err.println("进入service");
+	/*	MenuEntityExample menuEntityExample=new MenuEntityExample();
+		MenuEntityExample.Criteria criteria=menuEntityExample.createCriteria();
+		criteria.andIdEqualTo("123123");
+		List<MenuEntity> list =  getSqlSession().getMapper(MenuEntityMapper.class) .selectByExample(menuEntityExample); */
 		return null;
 	}
 
