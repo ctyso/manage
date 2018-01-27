241b5ac829134dd3adf0d4c90c0955cb25cbb4a6
ctyso <1204830623@qq.com>
2018-01-27 20:29:38 +0800

    菜单的service

---
+++ src/com/manage/sys/service/ISysMenuService.java
@@ -0,0 +1,7 @@
+package com.manage.sys.service;
+
+/**
+ * Created by 卢春文 on 2018/1/27.
+ */
+public interface ISysMenuService {
+}
---
+++ src/com/manage/sys/service/impl/SysMenuService.java
@@ -0,0 +1,18 @@
+package com.manage.sys.service.impl;
+
+import com.manage.base.MyBatisDao;
+import com.manage.sys.service.ISysMenuService;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+/**
+ * Created by 卢春文 on 2018/1/27.
+ */
+@Service
+@Transactional
+public class SysMenuService extends MyBatisDao implements ISysMenuService {
+    /*private static final String MAPPER_NAMESPACE = "com.manage.sys.sqlmapper.SysMenuMapper";*/
+
+    
+  
+}
