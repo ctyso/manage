5cf1a5ec53932836b6356022c63aa85b595cc353
ctyso <1204830623@qq.com>
2018-01-29 10:59:29 +0800

    数据库mapper扫描

--- src/com/manage/user/service/impl/UserService.java
+++ src/com/manage/user/service/impl/UserService.java
@@ -2,10 +2,10 @@ package com.manage.user.service.impl;
 
 import com.manage.base.MyBatisDao;
 import com.manage.sys.entity.SysMenuEntity;
-import com.manage.sys.entity.SysMenuEntityCondition;
-import com.manage.sys.entity.SysMenuEntityMapper;
 import com.manage.user.domain.User;
 import com.manage.user.service.IUserService;
+import org.apache.ibatis.session.SqlSession;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -14,16 +14,21 @@ import java.util.List;
 @Service
 @Transactional
 public class UserService extends MyBatisDao implements IUserService  {
+	public static final String MAPPER_NAMESPACE = "com.manage.sys.sqlmapper.SysMenuMapper";
 	
-
+	@Autowired
+	private SqlSession sqlsession;
 	
 	@Override
 	public User loginUser() {
-		SysMenuEntityCondition condition=new SysMenuEntityCondition();
+		List<SysMenuEntity> caseContentExtendList=sqlsession.selectList(MAPPER_NAMESPACE + ".findAll");
+
+		
+		/*SysMenuEntityCondition condition=new SysMenuEntityCondition();
 		SysMenuEntityCondition.Criteria criteria=condition.createCriteria();
 		criteria.andIdEqualTo("1232323");
 		condition.setOrderByClause("insert_time desc");
-		List<SysMenuEntity> list =  getSqlSession().getMapper(SysMenuEntityMapper.class) .selectByExample(condition);
+		List<SysMenuEntity> list =  getSqlSession().getMapper(SysMenuEntityMapper.class) .selectByExample(condition);*/
 		return null;
 	}
 
